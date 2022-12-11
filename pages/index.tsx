7ce9a1400dc6901096735985c6e574aa8b2b2121
shinyo17 <17shinyo17@gmail.com>
2022-12-11 23:02:10 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+export default function Home() {
+  return (
+    <div className="bg-slate-200 py-20 px-20 flex flex-col space-y-5 min-h-screen">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <ul>
+          {[1, 2, 3, 4].map((i) => (<div className="flex justify-between my-2">
+            <span className="text-gray-500 my-2">Grey Chair</span>
+            <span className="font-semibold">$19</span>
+          </div>))}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((c, i) => <li className="bg-red-500 py-2 empty:hidden" key={i}>{c}</li>)}
+        </ul>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$99</span>
+        </div>
+        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+          Checkout
+        </button>
+      </div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl group">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-zinc-400 rounded-full group-hover:bg-red-400 transition-colors" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spend</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+
+          </div>
+          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
+            <span className="text-lg font-medium">Tony Molly</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="shadow-xl p-2 ronded-md">💖</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium mb-1.5 text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="p-1.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
+              <span className="">1</span>
+              <button className="p-1.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">+</button>
+            </div>
+          </div>
+
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="font-medium text-2xl">$450</span>
+          <button className="bg-blue-500 text-center text-xs text-white rounded-lg px-5 py-2">Add to cart</button>
+        </div>
+      </div>
+    </div >
+  );
+}
